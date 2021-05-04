4c840bb8fa3f8d552762a1c907c21f2b86935796
Ercan Tomac <48109156+ercantomac@users.noreply.github.com>
2021-05-05 00:54:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ for (var i = 0; i < 6; i++) {
 }
 function onLoad() {
     document.body.animate({
-        backdropFilter: ["none"]
+        backdropFilter: ["none"],
+        webkitBackdropFilter: ["none"]
     }, {
         duration: 1200,
         easing: "ease",
@@ -16,7 +17,7 @@ function onLoad() {
     header.animate({
         transform: ["translateY(-6vw)", "translateY(0)"]
     }, {
-        duration: 800,
+        duration: 1000,
         easing: "ease",
         fill: "forwards",
         delay: 1500
@@ -25,18 +26,27 @@ function onLoad() {
         card[i].animate({
             transform: [(((i == 0 || i == 3) ? "scale(2.5) translateX(-28vw)" : ((i == 2 || i == 5) ? "scale(2.5) translateX(28vw)" : "scale(2.5) translateX(0)"))), "scale(1) translateX(0)"]
         }, {
-            duration: 700,
+            duration: 1000,
             easing: "cubic-bezier(0.215, 0.610, 0.355, 1)",
             fill: "forwards",
-            delay: (i < 3) ? (2300 + (i * 100)) : (2300 + (i * 100) + 300)
+            delay: (i < 3) ? (2500 + (i * 100)) : (2500 + (i * 100) + 400)
         });
         card[i].animate({
             opacity: ["0", "1"]
         }, {
-            duration: 700,
+            duration: 1000,
             easing: "ease",
             fill: "forwards",
-            delay: (i < 3) ? (2300 + (i * 100) + 100) : (2300 + (i * 100) + 400)
+            delay: (i < 3) ? (2500 + (i * 100)) : (2500 + (i * 100) + 400)
+        });
+        card[i].animate({
+            backdropFilter: ["blur(16px) brightness(110%)"],
+            webkitBackdropFilter: ["blur(16px) brightness(110%)"]
+        }, {
+            duration: 1000,
+            easing: "ease",
+            fill: "forwards",
+            delay: 4200
         });
     }
     setTimeout(() => {
@@ -44,7 +54,7 @@ function onLoad() {
             card[i].setAttribute("onmouseenter", "hoverAnimation(wrap[" + i + "], card[" + i + "], inner[" + i + "], 1)");
             card[i].setAttribute("onmouseleave", "hoverAnimation(wrap[" + i + "], card[" + i + "], inner[" + i + "], 0)");
         }
-    }, 3600);
+    }, 4200);
 }
 function hoverAnimation(wrap, el, inner, cnt) {
     wrap.animate({
@@ -59,13 +69,13 @@ function hoverAnimation(wrap, el, inner, cnt) {
         backgroundColor: [((cnt == 1) ? "transparent" : "rgba(255, 255, 255, .02)")],
         fontSize: [((cnt == 1) ? "2.75vw" : "2.25vw")],
         boxShadow: [((cnt == 1) ? "0 1vw 1.5vw -.15vw rgba(0, 0, 0, .625)" : "0 .25vw .5vw -.15vw rgba(0, 0, 0, .625)")],
-        border: [((cnt == 1) ? "1px solid rgba(0, 0, 0, .8)" : "1px solid rgba(255, 255, 255, .2)")]
+        border: [((cnt == 1) ? "1px solid rgba(0, 0, 0, .8)" : "1px solid rgba(255, 255, 255, .2)")],
+        textShadow: [((cnt == 1) ? "0 .2vw .15vw rgba(0, 0, 0, .8)" : "0 .1vw .15vw rgba(0, 0, 0, .625)")]
     }, {
         duration: 600,
         easing: "ease",
         fill: "forwards"
     });
-    el.style.textShadow = ((cnt == 1) ? "0 .2vw .15vw rgba(0, 0, 0, .8)" : "0 .15vw .15vw rgba(0, 0, 0, .8)");
     inner.animate({
         width: [((cnt == 1) ? "100%" : "0")]
     }, {
